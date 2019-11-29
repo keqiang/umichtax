4d41c429e6b46113cb77e8f3f2a0e6b0ed420050
Keqiang Li <keqianglli@gmail.com>
2019-11-28 19:06:21 -0500

    added marital status

--- app.R
+++ app.R
@@ -1,22 +1,34 @@
 library(shiny)
-
-options(digits = 2)
+library(shinydashboard)
 
 fedTaxBrackets <- list(
-  list(cutoff = 3800, rate = 0.1, base = 0),
-  list(cutoff = 13500, rate = 0.12, base = 970),
-  list(cutoff = 43275, rate = 0.22, base = 4543),
-  list(cutoff = 88000, rate = 0.24, base = 14382.5),
-  list(cutoff = 164525, rate = 0.32, base = 32748.5),
-  list(cutoff = 207900, rate = 0.35, base = 46628.5),
-  list(cutoff = 514100, rate = 0.37, base = 153798.5)
+  "Single" = list(
+    list(cutoff = 3800, rate = 0.1, base = 0),
+    list(cutoff = 13500, rate = 0.12, base = 970),
+    list(cutoff = 43275, rate = 0.22, base = 4543),
+    list(cutoff = 88000, rate = 0.24, base = 14382.5),
+    list(cutoff = 164525, rate = 0.32, base = 32748.5),
+    list(cutoff = 207900, rate = 0.35, base = 46628.5),
+    list(cutoff = 514100, rate = 0.37, base = 153798.5)
+  ),
+  "Married" = list(
+    list(cutoff = 11800, rate = 0.1, base = 0),
+    list(cutoff = 31200, rate = 0.12, base = 1940),
+    list(cutoff = 90750, rate = 0.22, base = 9086),
+    list(cutoff = 180200, rate = 0.24, base = 28765),
+    list(cutoff = 333250, rate = 0.32, base = 65497),
+    list(cutoff = 420000, rate = 0.35, base = 93257),
+    list(cutoff = 624150, rate = 0.37, base = 164709.50)
+  )
 )
 
 calculateFedTax <- function(taxableAmount, maritalStatus = c("Single", "Married")) {
   maritalStatus <- match.arg(maritalStatus)
 
-  for (i in rev(seq_along(fedTaxBrackets))) {
-    bracket <- fedTaxBrackets[[i]]
+  taxBrackets <- fedTaxBrackets[[maritalStatus]]
+
+  for (i in rev(seq_along(taxBrackets))) {
+    bracket <- taxBrackets[[i]]
     if (taxableAmount >= bracket$cutoff) {
       bracketTax <- (taxableAmount - bracket$cutoff) * bracket$rate
       return(bracketTax + bracket$base)
@@ -26,74 +38,133 @@ calculateFedTax <- function(taxableAmount, maritalStatus = c("Single", "Married"
   return(0)
 }
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-  theme = shinythemes::shinytheme("superhero"),
-  # Application title
-  titlePanel("University of Michigan Tax Calculator"),
+formatNumber <- function(number, decimals = 0) {
+  paste0("$", format(round(number, decimals), nsmall = decimals))
+}
 
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      helpText("EFFECTIVE January 1, 2019"),
-      radioButtons(
-        "payrollType",
-        label = "Payroll Type",
-        choices = c("Monthly", "Biweekly"),
-        selected = "Monthly",
-        inline = TRUE
-      ),
-      numericInput(
-        "perPayrollGross",
-        label = "Per-payroll Gross",
-        value = 4500,
-        min = 500,
-        step = 1
-      ),
-      helpText("'TOTAL GROSS' on your paystub"),
-      tags$hr(),
-      radioButtons(
-        "maritalStatus",
-        label = "Maital Status",
-        choices = c("Single", "Married"),
-        selected = "Single",
-        inline = TRUE
-      ),
-      numericInput(
-        "numOfExemptions",
-        label = "Number of Exemptions",
-        value = 0,
-        min = 0,
-        step = 1
-      ),
-      helpText("Displayed at the top right of your paystub. Each exemption claimed will result in a $4,200 deduction annually."),
-      tags$hr(),
-      numericInput(
-        "perPayrollBeforeTaxDeductions",
-        label = "Per-payroll Before-tax Deductions",
-        value = 0,
-        min = 0,
-        step = 1
-      ),
-      helpText("Sum of all your before-tax deductions per-payroll. For example, health care, retirement plan and FSA."),
-      tags$hr(),
-      numericInput(
-          "perPayrollSST",
-          label = "Per-payroll Social Security Tax",
-          value = 0,
-          min = 0,
-          step = 1
-      ),
-      helpText("Sum of 'Fed MED/EE' and Fed 'OASDI/EE' listed on your payroll."),
-      tags$hr()
+ui <- dashboardPage(
+  dashboardHeader(title = "UofM Tax Calculator"),
+  dashboardSidebar(disable = TRUE),
+  dashboardBody(
+    tags$h4(
+      style = "color: red;",
+      "EFFECTIVE January 1, 2019. The result is only an estimation of your tax. Please use with caution."
     ),
-
-    # Show a plot of the generated distribution
-    mainPanel(
-      textOutput("ftgInfo"),
-      textOutput("ftgYearlyAfterExemptionsInfo"),
-      textOutput("fedTaxYearlyInfo"),
-      textOutput("netPayInfo")
+    helpText(
+      "This website is based on http://www.finance.umich.edu/news/calculating-your-taxes"
+    ),
+    fluidRow(
+      column(
+        width = 6,
+        fluidRow(
+          box(
+            width = 12,
+            status = "warning",
+            title = "Pay Stub Information",
+            fluidRow(
+              column(
+                width = 6,
+                radioButtons(
+                  "payrollType",
+                  label = "Payroll Type",
+                  choices = c("Monthly", "Biweekly"),
+                  selected = "Monthly",
+                  inline = TRUE
+                )
+              ),
+              column(
+                width = 6,
+                radioButtons(
+                  "maritalStatus",
+                  label = "Maital Status",
+                  choices = c("Single", "Married"),
+                  selected = "Single",
+                  inline = TRUE
+                )
+              )
+            ),
+            numericInput(
+              "fedTaxableGross",
+              label = "Fed Taxable Gross (not total gross)",
+              value = 4500,
+              min = 0,
+              step = 1
+            ),
+            helpText("Displayed as 'FED TAXABLE GROSS' in the Paycheck Summary portion of your pay stub"),
+            tags$hr(),
+            numericInput(
+              "ficaTaxableGross",
+              label = "FICA Taxable Gross",
+              value = 4000,
+              min = 0,
+              step = 1
+            ),
+            helpText("Displayed as 'FICA TAXABLE GROSS' in the Paycheck Summary portion of your pay stub"),
+            tags$hr(),
+            numericInput(
+              "numOfExemptions",
+              label = "Number of Exemptions",
+              value = 0,
+              min = 0,
+              step = 1
+            ),
+            helpText("Displayed at the top right of your pay stub. Each exemption claimed will result in a $4,200 deduction annually."),
+            tags$hr(),
+            numericInput(
+              "afterTaxDeductions",
+              label = "After-tax Deductions",
+              value = 0,
+              min = 0,
+              step = 1
+            ),
+            helpText("Displayed at the center of your pay stub")
+          )
+        )
+      ),
+      column(
+        width = 6,
+        fluidRow(
+          box(
+            width = 12,
+            status = "success",
+            title = "Income Summary",
+            fluidRow(
+              valueBoxOutput("fedTaxableGross"),
+              valueBoxOutput("annualFedTaxableGrossAfterExemptionsInfo"),
+              valueBoxOutput("netPayInfo")
+            )
+          )
+        ),
+        fluidRow(
+          box(
+            width = 12,
+            status = "danger",
+            title = "Federal Tax",
+            fluidRow(
+              valueBoxOutput("annualFedTaxInfo", width = 6),
+              valueBoxOutput("perPayrollFexTaxInfo", width = 6)
+            )
+          ),
+          box(
+            width = 12,
+            status = "warning",
+            title = "State Tax",
+            fluidRow(
+              valueBoxOutput("annualStateTaxInfo", width = 6),
+              valueBoxOutput("perPayrollStateTaxInfo", width = 6)
+            )
+          ),
+          box(
+            width = 12,
+            status = "danger",
+            title = "FICA Tax",
+            fluidRow(
+              valueBoxOutput("socialSecurityTaxInfo", width = 6),
+              valueBoxOutput("medicareTaxInfo", width = 6)
+            )
+          )
+        )
+      )
     )
   )
 )
@@ -101,60 +172,122 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   payrollNums <- reactive({
-    ifelse(input$payrollType == "Monthly", 12, 24)
+    ifelse(input$payrollType == "Monthly", 12, 26)
   })
 
-  # Per-payroll Federal Taxable Gross
-  ftg <- reactive({
-    req(input$perPayrollGross, input$perPayrollBeforeTaxDeductions)
-    input$perPayrollGross - input$perPayrollBeforeTaxDeductions
+  output$fedTaxableGross <- renderValueBox({
+    valueBox(
+      formatNumber(input$fedTaxableGross),
+      subtitle = "Per-payroll federal taxable gross"
+    )
   })
 
-  output$ftgInfo <- renderText({
-    glue::glue("Your per-payroll Federal Taxable Gross is {ftg()}")
+  annualFedTaxableGross <- reactive({
+    input$fedTaxableGross * payrollNums()
   })
 
-  ftgYearly <- reactive({
-    req(payrollNums())
-    ftg() * payrollNums()
+  annualFedTaxableGrossAfterExemptions <- reactive({
+    annualFedTaxableGross() - 4200 * input$numOfExemptions
   })
 
-  ftgYearlyAfterExemptions <- reactive({
-    req(input$numOfExemptions)
-    ftgYearly() - 4200 * input$numOfExemptions
+  output$annualFedTaxableGrossAfterExemptionsInfo <- renderValueBox({
+    valueBox(
+      formatNumber(annualFedTaxableGrossAfterExemptions()),
+      subtitle = "Annual taxable gross"
+    )
   })
 
-  output$ftgYearlyAfterExemptionsInfo <- renderText({
-    glue::glue("Your annual taxable gross is: {ftgYearlyAfterExemptions()}")
+  annualFedTax <- reactive({
+    calculateFedTax(annualFedTaxableGrossAfterExemptions(), input$maritalStatus)
   })
-  
-  fedTaxYearly <- reactive({
-      calculateFedTax(ftgYearlyAfterExemptions(), input$maritalStatus)
+
+  output$annualFedTaxInfo <- renderValueBox({
+    valueBox(
+      formatNumber(annualFedTax()),
+      subtitle = "Annual federal tax",
+      color = "red"
+    )
   })
- 
+
   perPayrollFedTax <- reactive({
-      fedTaxYearly() / payrollNums()
+    annualFedTax() / payrollNums()
   })
-  
+
   perPayrollStateDeduction <- reactive({
-      ifelse(input$payrollType == "Monthly", 366.66, 169.23)
+    ifelse(input$payrollType == "Monthly", 366.66, 169.23)
   })
-  
+
   perPayrollStateTax <- reactive({
-      0.0425 * (ftg() - perPayrollStateDeduction())
+    0.0425 * (input$fedTaxableGross - perPayrollStateDeduction())
   })
-  
-  output$fedTaxYearlyInfo <- renderText({
-      glue::glue("Your annual federal tax is: {fedTaxYearly()}; your per-payroll federal tax is {perPayrollFedTax()}; your per-payroll state tax is {perPayrollStateTax()}")
+
+  output$annualStateTaxInfo <- renderValueBox({
+    valueBox(
+      formatNumber(perPayrollStateTax() * payrollNums()),
+      subtitle = "Annual state tax",
+      color = "yellow"
+    )
   })
-  
-  
+
+  output$perPayrollFexTaxInfo <- renderValueBox({
+    valueBox(
+      formatNumber(perPayrollFedTax()),
+      subtitle = "Per-payroll federal tax",
+      color = "red"
+    )
+  })
+
+  output$perPayrollStateTaxInfo <- renderValueBox({
+    valueBox(
+      formatNumber(perPayrollStateTax()),
+      subtitle = "Per-payroll state tax",
+      color = "yellow"
+    )
+  })
+
+  annualFicaTaxable <- reactive({
+    input$ficaTaxableGross * payrollNums()
+  })
+
+  perPayrollSocialSecurityTax <- reactive({
+    min(annualFicaTaxable(), 132900) * 0.062 / payrollNums()
+  })
+
+  output$socialSecurityTaxInfo <- renderValueBox({
+    valueBox(
+      formatNumber(perPayrollSocialSecurityTax()),
+      subtitle = "Per-payroll social security tax",
+      color = "maroon"
+    )
+  })
+
+  perPayrollMedicareTax <- reactive({
+    tax <- annualFicaTaxable() * 0.0145
+    wagesOver <- annualFicaTaxable() - 200000
+    if (wagesOver > 0) {
+      tax <- tax + wagesOver * 0.009
+    }
+    tax / payrollNums()
+  })
+
+  output$medicareTaxInfo <- renderValueBox({
+    valueBox(
+      formatNumber(perPayrollMedicareTax()),
+      subtitle = "Per-payroll medicare tax",
+      color = "maroon"
+    )
+  })
+
   netPay <- reactive({
-      ftg() - perPayrollFedTax() - perPayrollStateTax() - input$perPayrollSST
+    input$fedTaxableGross - perPayrollFedTax() - perPayrollStateTax() - perPayrollSocialSecurityTax() - perPayrollMedicareTax() - input$afterTaxDeductions
   })
-  
-  output$netPayInfo <- renderText({
-      glue::glue("Your per-payroll net pay is: {netPay()}")
+
+  output$netPayInfo <- renderValueBox({
+    valueBox(
+      formatNumber(netPay()),
+      subtitle = "Per-payroll net pay",
+      color = "green"
+    )
   })
 }
 
